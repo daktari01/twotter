49630e1129e6e79307c777a1697e62309606a565
James Dindi <dindijjames@gmail.com>
2020-12-11 14:04:15 +0300

    Add twoots, and new components

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <UserProfile/>
+</template>
+
+<script>
+import UserProfile from './components/UserProfile'
+
+export default {
+  name: 'App',
+  components: {
+    UserProfile
+  },
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: 'daktari',
+        firstName: 'James',
+        lastName: 'Daktari',
+        email: 'daktari@gmail.com',
+        isAdmin: true
+      }
+    }
+  },
+  watch: {
+    followers(newFollowerCount, oldFollowerCount) {
+      if (oldFollowerCount < newFollowerCount) {
+        console.log(`${this.user.username} has gained a follower!`);
+      }
+    }
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`;
+    }
+  },
+  methods: {
+    followUser() {
+      this.followers++
+    }
+  },
+  mounted() {
+    this.followUser();
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  min-height: 100vh;
+  background-color: #F3F5FA;
+}
+</style>
